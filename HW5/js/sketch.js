311f547dc7b6ba65be8366fc2afbeb51b5ee6bbb
joeylavs <jl104493@umconnect.umt.edu>
2026-02-17 16:09:42 -0700

    Add pizza drawing and animation functionality

--- HW5/js/sketch.js
+++ HW5/js/sketch.js
@@ -1,1 +1,75 @@
 
+// FOOD ARRAYS
+
+let foodX = [];
+let foodY = [];
+let foodCount = 10; // number of pizzas on screen
+
+
+// ANIMATION ARRAYS
+
+let frames = [];
+let totalFrames = 4; // change this to match your images
+let frameIndex = 0;
+
+function preload() {
+  // Load animation frames from images folder
+  for (let i = 0; i < totalFrames; i++) {
+    frames[i] = loadImage(`images/frame_${i}.png`);
+  }
+}
+
+function setup() {
+  createCanvas(600, 400);
+
+  // Create random pizza positions
+  for (let i = 0; i < foodCount; i++) {
+    foodX[i] = random(width);
+    foodY[i] = random(height);
+  }
+}
+
+function draw() {
+  background(230);
+
+  
+  // DRAW MULTIPLE PIZZAS
+  
+  for (let i = 0; i < foodCount; i++) {
+    drawPizza(foodX[i], foodY[i]);
+  }
+
+  
+  // DRAW ANIMATION
+  
+  image(frames[frameIndex], 400, 200, 120, 120);
+
+  // Cycle animation frames
+  frameIndex++;
+  if (frameIndex >= frames.length) {
+    frameIndex = 0;
+  }
+}
+
+
+// SIMPLE PIZZA DRAWING FUNCTION
+
+function drawPizza(x, y) {
+  push();
+  translate(x, y);
+
+  // crust
+  fill(210, 160, 80);
+  circle(0, 0, 40);
+
+  // sauce
+  fill(200, 50, 50);
+  circle(0, 0, 30);
+
+  // cheese
+  fill(255, 220, 100);
+  circle(0, 0, 25);
+
+  pop();
+}
+
