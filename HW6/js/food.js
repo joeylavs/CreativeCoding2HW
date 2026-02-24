44f8020b8237b2d97e548d073b642f0247a5ea58
joeylavs <jl104493@umconnect.umt.edu>
2026-02-23 18:34:37 -0700

    Add Food class with display method

--- HW6/js/food.js
+++ HW6/js/food.js
@@ -1,1 +1,26 @@
+class Food {
+  constructor(x, y, size, col) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.col = col;
+  }
+
+  display() {
+    push();
+    translate(this.x, this.y);
+    noStroke();
+    fill(this.col);
+
+    // shape 1: main body (circle)
+    ellipse(0, 0, this.size);
+
+    // shape 2: topping (rectangle)
+    fill(255, 255, 0);
+    rectMode(CENTER);
+    rect(0, -this.size * 0.2, this.size * 0.6, this.size * 0.2);
+
+    pop();
+  }
+}
 
