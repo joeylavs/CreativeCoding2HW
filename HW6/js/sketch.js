33e92f47cd1f316025290e0eccd5566b64469a56
joeylavs <jl104493@umconnect.umt.edu>
2026-02-23 18:34:08 -0700

    Update sketch.js

--- HW6/js/sketch.js
+++ HW6/js/sketch.js
@@ -1,1 +1,19 @@
+let idleFrames = [];
+let walkFrames = [];
+let currentAnimation = "idle";
+let frameIndex = 0;
+let frameTimer = 0;
+let frameDelay = 8; // bigger = slower
 
+let charX = 200;
+let charY = 200;
+let charSpeed = 2;
+
+function preload() {
+  for (let i = 0; i < 3; i++) {
+    idleFrames.push(loadImage(`images/idle_${i}.png`));
+  }
+  for (let i = 0; i < 4; i++) {
+    walkFrames.push(loadImage(`images/walk_${i}.png`));
+  }
+}
