b787c473580d1f27e92c64d85cd69fd691fac0be
joeylavs <jl104493@umconnect.umt.edu>
2026-04-20 15:37:01 -0600

    Update collectible.js

--- HW14/collectible.js
+++ HW14/collectible.js
@@ -1,1 +1,17 @@
+class Collectible {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 20;
+  }
+
+  display() {
+    fill(50, 200, 255);
+    ellipse(this.x, this.y, this.size);
+  }
+
+  collected(player) {
+    return dist(this.x, this.y, player.x, player.y) < (this.size + player.size) / 2;
+  }
+}
 
