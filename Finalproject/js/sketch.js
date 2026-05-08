b0cccac3f8b34171ad0c816c87e0c63e7236eb60
joeylavs <jl104493@umconnect.umt.edu>
2026-05-07 19:13:31 -0600

    Update sketch.js

--- Finalproject/js/sketch.js
+++ Finalproject/js/sketch.js
@@ -4,45 +4,64 @@ let treeGroup, rockGroup;
 let playerImg, wendigoImg, treeImg, rockImg;
 
 function preload() {
-  playerImg = loadImage("assets/player/player1.png");
-  wendigoImg = loadImage("assets/enemy/wendigo1.png");
-  treeImg = loadImage("assets/images/tree1.png");
-  rockImg = loadImage("assets/images/rock1.png");
+  playerImg = loadImage("assets/player/player.png");
+  wendigoImg = loadImage("assets/enemy/wendigo.png");
+  treeImg = loadImage("assets/obstacles/tree.png");
+  rockImg = loadImage("assets/obstacles/rock.png");
 }
 
 function setup() {
   createCanvas(800, 600);
 
-  // Player
+  // Debug: confirm p5play loaded
+  console.log("Sprite test:", createSprite);
+
+  // PLAYER ----------------------------------------------------
   player = new Sprite(width / 2, height - 100);
   player.img = playerImg;
-  player.scale = 0.15;
-  player.collider = "none"; // no physics body
 
-  // Wendigo
+  // Correct size for 16x16 pixel art
+  player.w = 20;
+  player.h = 20;
+
+  player.collider = "none";
+
+  // WENDIGO ---------------------------------------------------
   wendigo = new Sprite(width / 2, 100);
   wendigo.img = wendigoImg;
-  wendigo.scale = 0.2;
+
+  // Slightly bigger than player
+  wendigo.w = 28;
+  wendigo.h = 28;
+
   wendigo.collider = "none";
 
-  // Groups
+  // GROUPS ----------------------------------------------------
   treeGroup = new Group();
   rockGroup = new Group();
 
-  // Trees
+  // TREES -----------------------------------------------------
   for (let i = 0; i < 5; i++) {
     let t = new Sprite(random(100, width - 100), random(150, height - 200));
     t.img = treeImg;
-    t.scale = .4;
+
+    // Good tree size
+    t.w = 40;
+    t.h = 60;
+
     t.collider = "static";
     treeGroup.add(t);
   }
 
-  // Rocks
+  // ROCKS -----------------------------------------------------
   for (let i = 0; i < 5; i++) {
     let r = new Sprite(random(100, width - 100), random(150, height - 200));
     r.img = rockImg;
-    r.scale = .4;
+
+    // Good rock size
+    r.w = 30;
+    r.h = 20;
+
     r.collider = "static";
     rockGroup.add(r);
   }
@@ -51,13 +70,13 @@ function setup() {
 function draw() {
   background(0);
 
-  // Player movement
+  // PLAYER MOVEMENT -------------------------------------------
   if (kb.pressing("left")) player.x -= 4;
   if (kb.pressing("right")) player.x += 4;
   if (kb.pressing("up")) player.y -= 4;
   if (kb.pressing("down")) player.y += 4;
 
-  // Collision detection (v3 syntax)
+  // COLLISION CHECK -------------------------------------------
   if (player.overlaps(treeGroup) || player.overlaps(rockGroup)) {
     moveWendigoCloser();
   }
